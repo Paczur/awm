890eef92d7d1be3d6cd9f2f75b0589c8abc9038a
Paczur <149335135+Paczur@users.noreply.github.com>
2024-08-17 20:02:35 +0200

    Change font and icons for new look

--- src/user_config.h
+++ src/user_config.h
@@ -104,8 +104,6 @@
 #define CONFIG_BAR_BACKGROUND DARK_GRAY
 #define CONFIG_BAR_HEIGHT 32
 #define CONFIG_BAR_FONT "Maple Mono 14"
-#undef CONFIG_BAR_FONT
-#define CONFIG_BAR_FONT "CodeNewRoman Nerd Font Propo 14"
 #define CONFIG_BAR_COMPONENT_PADDING 8
 #define CONFIG_BAR_COMPONENT_SEPARATOR 2
 
@@ -135,12 +133,12 @@
 // clang-format off
 #define CONFIG_BAR_MINIMIZED_NAME_REPLACEMENTS                 \
   {                                                            \
-    {"mlterm",           "󰆍"}, \
+    {"mlterm",           ""}, \
     {"librewolf",        ""}, \
     {"LibreWolf",        ""}, \
     {"gimp",             ""}, \
     {"telegram-desktop", ""}, \
-    {"mpv",              ""}, \
+    {"mpv",              ""}, \
     {"feh",              ""}  \
   }
 // clang-format on
@@ -162,8 +160,8 @@
   {4, "/etc/awm/scripts/battery",    60}, \
   {8, "/etc/awm/scripts/gpu",        60}, \
   {5, "/etc/awm/scripts/cpu",        5},  \
-  {6, "date '+󰃶  %a %d' && exit 1",  3600}, \
-  {7, "date '+  %H:%M' && exit 1",  30}, \
+  {6, "date '+󰃶 %a %d' && exit 1",  3600}, \
+  {7, "date '+ %H:%M' && exit 1",  30}, \
 }
 // clang-format on
 
