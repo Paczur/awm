8a239dd22387d121bdfb24da5a4ec4335742047f
Paczur <149335135+Paczur@users.noreply.github.com>
2025-04-05 21:54:51 +0200

    Cleanup

--- old/event.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_EVENT
-#define H_EVENT
-
-#include <xcb/xcb.h>
-
-void event_listener_add(uint8_t, void (*)(const xcb_generic_event_t*));
-void event_next(xcb_connection_t*);
-
-#endif
--- test-old/shortcut/shortcut.h
+++
@@ -1,8 +0,0 @@
-#ifndef H_AWM_TEST_SHORTCUT
-#define H_AWM_TEST_SHORTCUT
-
-#include <ctf/ctf.h>
-
-CTF_GROUP_EXTERN(shortcut_spec);
-
-#endif
